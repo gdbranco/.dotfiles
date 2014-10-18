716359e97be2749acedab774347bb985f1e1c7f6
Shurd <gdbranco@gmail.com>
2014-10-17 23:27:44 -0300

    cfg fix

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 cp -rf ./.vim ~/
 cp .vimrc ~/.vimrc
+cp .bash_aliases ~/.bash_aliases
+cp .zshrc ~/.zshrc
+cp .tmux.conf ~/.tmux.conf
+cp .bashrc ~/.bashrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
