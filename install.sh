1587f4533ff6d3e5f612fe45c3b5f2227ce199f9
Shurd <gdbranco@gmail.com>
2014-10-18 11:04:37 -0300

    fonte

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ cp .bash_aliases ~/.bash_aliases
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
 cp .bashrc ~/.bashrc
+cp .snapshot ~/.snapshot
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+#xdg-open ubuntu_mono_derivative_powerline.ttf
+cp ubuntu_mono_derivative_powerline.ttf ~/.fonts
+fc -cache -v
